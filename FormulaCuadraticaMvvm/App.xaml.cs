3a499162f901f57379f56c77e7b758f8e578636d
MoiPad92 <moipad85@gmail.com>
2024-03-13 16:23:26 -0600

    Agregar archivos de proyecto.

---
+++ FormulaCuadraticaMvvm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FormulaCuadraticaMvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FormulaCuadraticaMvvm/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FormulaCuadraticaMvvm
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

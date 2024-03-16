f09ae213aa712f19fa49461430e985b59ba1ea36
MoiPad92 <moipad85@gmail.com>
2024-03-16 09:08:23 -0600

    Configuración de la fuente de FontAwesome

--- FormulaCuadraticaMvvm/MauiProgram.cs
+++ FormulaCuadraticaMvvm/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace FormulaCuadraticaMvvm
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("fa-Regular-400.ttf", "FaRegular");
+                    
                 });
 
 #if DEBUG
